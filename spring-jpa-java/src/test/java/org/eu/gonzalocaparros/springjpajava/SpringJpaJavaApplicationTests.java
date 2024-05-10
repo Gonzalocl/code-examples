7a9c33f6aa31dec3ded02069c06bba8d42301113
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2024-05-11 00:37:20 +0200

    Use spaces

--- spring-jpa-java/src/test/java/org/eu/gonzalocaparros/springjpajava/SpringJpaJavaApplicationTests.java
+++ spring-jpa-java/src/test/java/org/eu/gonzalocaparros/springjpajava/SpringJpaJavaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringJpaJavaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
