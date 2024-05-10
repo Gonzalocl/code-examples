db3e73218cdec24222563f1993b700c29d859134
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2024-05-11 00:37:20 +0200

    Use lombok

--- spring-jpa-java/src/main/java/org/eu/gonzalocaparros/springjpajava/Note.java
+++ spring-jpa-java/src/main/java/org/eu/gonzalocaparros/springjpajava/Note.java
@@ -3,45 +3,22 @@ package org.eu.gonzalocaparros.springjpajava;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class Note {
 
+    @Id
+    @GeneratedValue
     private Long id;
     private String title;
     private String description;
 
-    protected Note() {
-    }
-
     public Note(String title, String description) {
         this.title = title;
-        this.description = description;
-    }
-
-    @Id
-    @GeneratedValue
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
         this.description = description;
     }
 }
