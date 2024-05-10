67ee5faed47b9d34bcffdf420a2436f29765fa92
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2024-05-11 00:37:20 +0200

    Init spring-jpa-java

---
+++ spring-jpa-java/src/main/java/org/eu/gonzalocaparros/springjpajava/SpringJpaJavaApplication.java
@@ -0,0 +1,13 @@
+package org.eu.gonzalocaparros.springjpajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaJavaApplication.class, args);
+	}
+
+}
---
+++ spring-jpa-java/src/test/java/org/eu/gonzalocaparros/springjpajava/SpringJpaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eu.gonzalocaparros.springjpajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
