74b6bd74ef5bce08183f4411b98ef7f78b859423
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2024-05-11 00:37:20 +0200

    Add mapstruct

--- spring-rest-java/src/main/java/org/eu/gonzalocaparros/example/notes/io/rest/mapper/NoteMapper.java
+++ spring-rest-java/src/main/java/org/eu/gonzalocaparros/example/notes/io/rest/mapper/NoteMapper.java
@@ -2,7 +2,9 @@ package org.eu.gonzalocaparros.example.notes.io.rest.mapper;
 
 import org.eu.gonzalocaparros.example.notes.domain.model.Note;
 import org.eu.gonzalocaparros.example.notes.io.rest.dto.NoteDto;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface NoteMapper {
     Note from(NoteDto dto);
     NoteDto from(Note note);
