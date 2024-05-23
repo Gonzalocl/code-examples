c217fa7e056c68d7303d40ab6059fab4deb4bc29
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2024-05-23 08:30:25 +0200

    p3

--- kotlin-sqlite-sqldelight-jvm/src/main/kotlin/Main.kt
+++ kotlin-sqlite-sqldelight-jvm/src/main/kotlin/Main.kt
@@ -2,10 +2,21 @@ package org.eu.gonzalocaparros
 
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import org.eu.gonzalocaparros.sqldelight.hockey.data.Map
 
 fun main() {
-    println("Hello World!")
-    val driver: SqlDriver = JdbcSqliteDriver("jdbc:sqlite:test.db")
+    val driver: SqlDriver = JdbcSqliteDriver("jdbc:sqlite:db.sqlite")
     Database.Schema.create(driver)
+
+    val database = Database(driver)
+    val trackQueries = database.trackQueries
+
+    println(trackQueries.selectAll().executeAsList())
+
+    trackQueries.insert(id = 0, name = "Map #0")
+    println(trackQueries.selectAll().executeAsList())
+
+    val map = Map(1, "Map #1")
+    trackQueries.insertFullMapObject(map)
 
 }
